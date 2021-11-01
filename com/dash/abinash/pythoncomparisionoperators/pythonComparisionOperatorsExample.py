20f8055560510a3765ac7a453925378754d2d95d
Abinash Dash <xabidas@Abinashs-iMac.local>
2021-10-31 21:18:03 -0700

    Details about Python Comparison operators

---
+++ com/dash/abinash/pythoncomparisionoperators/pythonComparisionOperatorsExample.py
@@ -0,0 +1,38 @@
+print(2 == 2) # True
+
+print(2 == 1) # False
+
+print('hello' == 'bye') # False
+
+print('Bye' == 'bye') # False
+
+print('2' == 2) # False
+
+print(2.0 == 2) # True
+
+print(3 != 3) # False
+
+print(4 != 5) # True
+
+print(1 > 2) # False
+
+print(2 < 5) # True
+
+print(2 >= 2) # True
+
+print(2 <= 2) # True
+
+print(1 < 2 and 2 < 3) # True
+
+print(1 < 2 and 2 > 3) # False
+
+print('h' == 'h' and 2 == 2) # True
+
+print(100 == 1 or 2 == 2) # True
+
+print(1== 1) # True
+
+print(not 1 == 1) # False
+
+print(1 <= 2 >= 3) # False because 1 <= 2 (True) But 2 >= 3 (False).
+# Here we are basically checking the 'and' logical operator checking.
---
+++ com/dash/abinash/pythoncomparisionoperators/pythonComparisionOpertorsInfo.py
@@ -0,0 +1,5 @@
+# Python Logical Operators Comparison -
+# 1. We can use logical operators to combine comparisons.
+#       - and
+#       - or
+#       - not
